d5ff9b51f075d14def16f7c21c31438e71ab17c6
Eduardo Santamaria <edsanav@gmail.com>
2019-12-05 22:05:51 +0100

    WIP

--- src/main/scala/exercises/day3.scala
+++ src/main/scala/exercises/day3.scala
@@ -2,6 +2,23 @@ package exercises
 
 object day3 extends App {
 
+  trait Move{
+    val positions:Int
+  }
+
+  case class Up(positions:Int) extends Move
+  case class Down(positions:Int) extends Move
+  case class Left(positions:Int) extends Move
+  case class Right(positions:Int) extends Move
+
+
+
+  case class Point(x:Int, y:Int)
+
+  case class Line(p1:Point, p2:Point){
+    def apply(p1: Point, move:Move): Line =  Line(p1, p2)
+  }
+
   def run(input_file:String):Int = ???
 
   val result = run(args.headOption.getOrElse("inputs/day3.csv"))
