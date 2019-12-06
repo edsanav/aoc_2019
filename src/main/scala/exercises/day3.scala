cf1ced9fc6329955b9a9c90d528bf51d5841cc12
Eduardo Santamaria <edsanav@gmail.com>
2019-12-06 11:55:33 +0100

    WIP

--- src/main/scala/exercises/day3.scala
+++ src/main/scala/exercises/day3.scala
@@ -5,18 +5,63 @@ object day3 extends App {
   trait Move{
     val positions:Int
   }
+  object Move {
+    def fromString(s:String):Move =  {
+      val positions:Int = s.drop(1).toInt
+      s.charAt(0) match {
+        case 'U' => Up(positions)
+        case 'D' => Down(positions)
+        case 'R' => Right(positions)
+        case 'L' => Left(positions)
+      }
+    }
+  }
 
-  case class Up(positions:Int) extends Move
-  case class Down(positions:Int) extends Move
-  case class Left(positions:Int) extends Move
-  case class Right(positions:Int) extends Move
+  trait VerticalMove extends Move
+  trait HorizontalMove extends Move
 
-
+  case class Up(positions:Int) extends VerticalMove
+  case class Down(positions:Int) extends VerticalMove
+  case class Left(positions:Int) extends HorizontalMove
+  case class Right(positions:Int) extends HorizontalMove
 
   case class Point(x:Int, y:Int)
 
-  case class Line(p1:Point, p2:Point){
-    def apply(p1: Point, move:Move): Line =  Line(p1, p2)
+  trait Line{
+    val p1:Point
+    val p2:Point
+  }
+
+  case class Horizontal(y:Int, x1:Int, x2:Int) extends Line{
+    def apply(p:Point, m:HorizontalMove): Line = m match {
+      case m:Left => Horizontal(y=p.y, x1=p.x, x2=p.x-m.positions)
+      case m:Right => Horizontal(y=p.y, x1=p.x, x2=p.x+m.positions)
+    }
+    val p1=Point(x=x1, y=y)
+    val p2=Point(x=x2, y=y)
+
+    def crossPoint(l:Horizontal):Option[Point] = None
+    def crossPoint(l:Vertical):Option[Point] = {
+      if (x1 <= l.x  &&  l.x <= x2) Some(Point(l.x, y))
+      else None
+    }
+
+  }
+
+
+  case class Vertical(x:Int, y1:Int, y2:Int) extends  Line {
+    def apply(p:Point, m:VerticalMove): Line = m match {
+      case m:Up => Vertical(x=p.x, y1=p.y, y2=p.y+m.positions)
+      case m:Down => Vertical(x=p.x, y1=p.y, y2=p.y-m.positions)
+    }
+    val p1=Point(x=x, y=y1)
+    val p2=Point(x=x, y=y2)
+
+    def crossPoint(l:Vertical):Option[Point] = None
+    def crossPoint(l:Horizontal):Option[Point] = {
+      if (y1 <= l.y  &&  l.y <= y2) Some(Point(x, l.y))
+      else None
+    }
   }
 
   def run(input_file:String):Int = ???
