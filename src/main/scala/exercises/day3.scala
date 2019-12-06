f347a5e9cc5340e2df659ff3613dbab5a5c3f5fe
Eduardo Santamaria <edsanav@gmail.com>
2019-12-06 17:47:20 +0100

    Better pattern matching on day3

--- src/main/scala/exercises/day3.scala
+++ src/main/scala/exercises/day3.scala
@@ -27,7 +27,11 @@ object day3 extends App {
 
   case class Point(x:Int, y:Int)
 
-  trait Line{
+  object Line{
+    def unapply(l: Line): Option[(Point, Point)] = Some(l.p1, l.p2)
+  }
+
+  sealed trait Line{
     val p1:Point
     val p2:Point
   }
@@ -40,16 +44,15 @@ object day3 extends App {
     val p1=Point(x=x1, y=y)
     val p2=Point(x=x2, y=y)
 
-    def crossPoint(l:Horizontal):Option[Point] = None
-    def crossPoint(l:Vertical):Option[Point] = {
-      if (x1 <= l.x  &&  l.x <= x2) Some(Point(l.x, y))
-      else None
+    def crossPoint(l:Line):Option[Point] = l match {
+      case l:Vertical if (x1 <= l.x  &&  l.x <= x2) =>Some(Point(l.x, y))
+      case _ => None
     }
 
   }
 
-
   case class Vertical(x:Int, y1:Int, y2:Int) extends  Line {
+
     def apply(p:Point, m:VerticalMove): Line = m match {
       case m:Up => Vertical(x=p.x, y1=p.y, y2=p.y+m.positions)
       case m:Down => Vertical(x=p.x, y1=p.y, y2=p.y-m.positions)
@@ -57,11 +60,11 @@ object day3 extends App {
     val p1=Point(x=x, y=y1)
     val p2=Point(x=x, y=y2)
 
-    def crossPoint(l:Vertical):Option[Point] = None
-    def crossPoint(l:Horizontal):Option[Point] = {
-      if (y1 <= l.y  &&  l.y <= y2) Some(Point(x, l.y))
-      else None
+    def crossPoint(l:Line):Option[Point] = l match {
+      case l:Horizontal if (y1 <= l.y  &&  l.y <= y2) => Some(Point(x, l.y))
+      case _ => None
     }
+
   }
 
   def run(input_file:String):Int = ???
