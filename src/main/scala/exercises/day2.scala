e5076b2e541923628720621870bb7d998e6242e0
Eduardo Santamaria <edsanav@gmail.com>
2019-12-15 12:39:19 +0100

    WIP

--- src/main/scala/exercises/day2.scala
+++ src/main/scala/exercises/day2.scala
@@ -26,33 +26,53 @@ object day2 extends App{
   sealed trait Operation
 
   trait Action extends Operation {
-    def execute(v:Vector[Int]):Vector[Int]
-    val moveCursor:Int
+    def execute:Vector[Int]
+    def nextCursor(cursor:Int): Int
   }
 
-  case class Sum(p1: ReadParam, p2: ReadParam, result: Int) extends Action {
-    def execute(v:Vector[Int]):Vector[Int]  = v updated(result, p1(v) + p2(v))
-    val moveCursor:Int = 4
+  case class Sum(v:Vector[Int], p1: ReadParam, p2: ReadParam, result: Int) extends Action {
+    def execute:Vector[Int]  = v updated(result, p1(v) + p2(v))
+    def nextCursor(cursor:Int):Int = cursor + 4
   }
 
-  case class Multiplication(p1: ReadParam, p2: ReadParam, result: Int) extends Action {
-    def execute(v:Vector[Int]):Vector[Int]  = v updated(result, p1(v) * p2(v))
-    val moveCursor:Int = 4
+  case class Multiplication(v:Vector[Int], p1: ReadParam, p2: ReadParam, result: Int) extends Action {
+    def execute:Vector[Int]  = v updated(result, p1(v) * p2(v))
+    def nextCursor(cursor:Int):Int = cursor + 4
   }
 
-  case class InputAction(result:Int) extends Action {
-    def execute(v:Vector[Int]):Vector[Int]  = {
+  case class InputAction(v:Vector[Int], result:Int) extends Action {
+    def execute:Vector[Int]   = {
       v updated(result, readUserInput())
     }
-    val moveCursor:Int = 2
+    def nextCursor(cursor:Int):Int = cursor + 2
   }
 
-  case class OutputAction(p1:ReadParam) extends Action {
-    def execute(v:Vector[Int]):Vector[Int] = {
+  case class OutputAction(v:Vector[Int], p1:ReadParam) extends Action {
+    def execute:Vector[Int]  = {
       println(s"OUTPUT: ${p1(v)}")
       v
     }
-    val moveCursor:Int = 2
+    def nextCursor(cursor:Int):Int = cursor + 2
+  }
+
+  case class JumpIfTrue(v:Vector[Int], p1:ReadParam, p2:ReadParam) extends Action {
+    def execute:Vector[Int]  = ???
+    def nextCursor(cursor:Int):Int = cursor + 3 //CAREFUL!
+  }
+
+  case class JumpIfFalse(v:Vector[Int], p1:ReadParam, p2:ReadParam) extends Action {
+    def execute:Vector[Int]  = ???
+    def nextCursor(cursor:Int):Int = cursor + 2
+  }
+
+  case class LessThan(v:Vector[Int], p1:ReadParam, p2:ReadParam) extends Action {
+    def execute:Vector[Int]  = ???
+    def nextCursor(cursor:Int):Int = cursor + 2
+  }
+
+  case class Equals(v:Vector[Int], p1:ReadParam, p2:ReadParam) extends Action {
+    def execute:Vector[Int]  = ???
+    def nextCursor(cursor:Int):Int = cursor + 2
   }
 
   case object End extends Operation
@@ -68,10 +88,11 @@ object day2 extends App{
     val opCode = v(cursor)
     val op:Int = opCode % 100
     def rpF:Int => ReadParam = getReadParam(v, cursor)(opCode) // partially applied f to get ReadParam from opCursor
-    if (op == 1) Sum(rpF(1),  rpF(2), v(cursor+3))
-    else if (op == 2) Multiplication(rpF(1),  rpF(2), v(cursor+3))
-    else if (op == 3) InputAction(v(cursor+1))
-    else if (op == 4) OutputAction(rpF(1))
+    if (op == 1) Sum(v, rpF(1),  rpF(2), v(cursor+3))
+    else if (op == 2) Multiplication(v, rpF(1),  rpF(2), v(cursor+3))
+    else if (op == 3) InputAction(v, v(cursor+1))
+    else if (op == 4) OutputAction(v, rpF(1))
+    else if (op == 5) OutputAction(v, rpF(1))
     else if (op == 99) End
     else throw new IllegalArgumentException("Invalid operation")
   }
@@ -80,7 +101,7 @@ object day2 extends App{
   def operation(v:Vector[Int], cursor:Int):(Vector[Int], Int) = {
     translate(v, cursor) match {
       case End => (v, cursor)
-      case op:Action => operation(op.execute(v), cursor + op.moveCursor)
+      case op:Action => operation(op.execute, op.nextCursor(cursor))
     }
   }
 
