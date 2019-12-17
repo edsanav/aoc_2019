b45d53f569b2fa85f9c1895247bc104795cda3f6
Eduardo Santamaria <edsanav@gmail.com>
2019-12-17 22:43:39 +0100

    Day 6

--- src/main/scala/exercises/day6.scala
+++ src/main/scala/exercises/day6.scala
@@ -1,13 +1,26 @@
 package exercises
 import scala.collection.immutable.HashMap
 import scala.io.Source
+import scala.collection.immutable.Queue
 
 object day6 extends App{
 
-  case class Planet(code:String, orbiters:Set[Planet], distance:Int=0){
+  case class Planet(code:String, orbiters:HashMap[String, Planet], orbited:Queue[String]){
+    val checkSum:Int = orbiters.values.foldLeft(orbited.size)((accum, x)=> accum + x.checkSum)
 
+    def differentOrbits(p2:Planet):(Queue[String],Queue[String]) = {
+      val (common, onp1) = orbited.partition(s => p2.orbited.contains(s))
+      val onp2 = p2.orbited.dropWhile(common.contains)
+      (onp1, onp2)
+    }
 
-    val checkSum:Int = orbiters.foldLeft(distance)((accum, x)=> accum + x.checkSum)
+    def get(c:String):Option[Planet] = {
+      orbiters.get(c) match {
+        case Some(x) => Some(x)
+        case None => orbiters.values.flatMap(x => x.get(c)).find{p:Planet => true}
+      }
+    }
+
   }
 
   def splitOrbit(orbitStr:String):(String, String) = {
@@ -26,14 +39,24 @@ object day6 extends App{
   }
 
 
-  def toPlanet(code:String, orbitMap:HashMap[String, Set[String]], distance:Int=0):Planet = {
-    Planet(code, orbitMap(code).foldLeft(Set[Planet]())((total, x)=> total + toPlanet(x, orbitMap, distance+1)), distance)
+  def toPlanet(code:String, orbitMap:HashMap[String, Set[String]], orbited:Queue[String]):Planet = {
+    Planet(
+      code,
+      orbitMap(code).foldLeft(HashMap[String, Planet]()) { (total, x) =>
+        val planet = toPlanet(x, orbitMap, orbited :+ code)
+        total + (planet.code -> planet)
+      },
+      orbited
+    )
   }
 
   def run(input_file:String):(Int,Int) = {
     val orbitMap = organizeOrbits(Source.fromResource(input_file).getLines)
-    val wholeSystem = toPlanet("COM", orbitMap)
-    (wholeSystem.checkSum, 0)
+    val wholeSystem = toPlanet("COM", orbitMap, Queue[String]())
+    val you = wholeSystem.get("YOU").get
+    val santa = wholeSystem.get("SAN").get
+    val (yourOrbits,santaOrbits) =  you.differentOrbits(santa)
+    (wholeSystem.checkSum, yourOrbits.size+santaOrbits.size)
   }
 
 
