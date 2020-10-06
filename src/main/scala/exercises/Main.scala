c10e0c96a22fd0830d6bfb9d40b0d433c41410e4
Eduardo Santamaria <edsanav@gmail.com>
2020-10-06 22:43:12 +0200

    Ex1 and some main modifications

--- src/main/scala/exercises/Main.scala
+++ src/main/scala/exercises/Main.scala
@@ -4,13 +4,15 @@ import cats.effect._
 
 object Main extends IOApp {
 
+  def runModule(id:String):IO[Int] = id match {
+    case "1" => ex1.run
+    case _ => IO.raiseError(new IllegalArgumentException("Invalid week number"))
+  }
+
   override def run(args: List[String]): IO[ExitCode] =
-    for {
-      _  <- if(args.length < 1) IO.raiseError(new IllegalArgumentException("Need week number to execute"))
-            else IO.unit
-      _ <- args.head match {
-            case "1" => ex1.run
-            case _ => IO.raiseError(new IllegalArgumentException("Invalid week number"))
-      }
-    } yield ExitCode.Success
+    args.headOption match {
+        case None => IO(System.err.println("Usage: run <week number>")).as(ExitCode(2))
+        case Some(module) => runModule(module).map(out => println(s"Result is $out")).as(ExitCode.Success)
+    }
+
 }
--- src/main/scala/exercises/ex1.scala
+++ src/main/scala/exercises/ex1.scala
@@ -1,19 +1,42 @@
 package exercises
 
 import cats.effect._
-
+import cats.Foldable
+import cats.instances.list._
+import cats.syntax.applicative._
 import auxiliar._
+
+import scala.annotation.tailrec
 
 object ex1 {
 
   val INPUT:String = "inputs/day1.csv"
 
-  def run: IO[ExitCode] = {
+  def run: IO[Int] = {
     for {
       lines <- loadResourceFile(INPUT).use(getLines)
       values <- IO(lines.map(_.toInt))
-      _ <- IO(println(values))
-    } yield ExitCode.Success
+      result <-IO(sumFuel(values))
+    } yield result
+  }
+
+  def getFuel(mass:Int):Int = (mass.toDouble / 3 ).floor.toInt - 2
+
+  def sumFuel[F[_]: Foldable](masses:List[Int]):Int =
+    masses.foldLeft(0){case (z, b) =>
+      val fuel = getFuel(b)
+      val extra = computeExtra(fuel)
+      z + fuel + extra
+    }
+
+  def computeExtra(fuelMass:Int):Int = {
+    @tailrec
+    def go(remaining: Int, accum:Int):Int = {
+      val fuelForFuel = getFuel(remaining)
+      if (fuelForFuel > 0) go(fuelForFuel, accum+fuelForFuel)
+      else accum
+    }
+    go(fuelMass, 0)
   }
 
 
