6aacf4a5bf8d710dd5685b19052e842fbc0766d0
Eduardo Santamaria <edsanav@gmail.com>
2020-10-28 23:32:38 +0100

    Ey at least compiles

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.13.1"
 
 
 libraryDependencies += "org.typelevel" %% "cats-effect" % "2.2.0"
+libraryDependencies += "org.typelevel" %% "cats-mtl" % "1.0.0"
--- src/main/scala/exercises/computer.scala
+++ src/main/scala/exercises/computer.scala
@@ -1,6 +1,9 @@
 package exercises
 
 import cats.data.{EitherT, State}
+import cats.{Applicative, Id, Monad, MonadError}
+import cats.implicits._
+import cats.mtl.{Raise, Stateful}
 
 object computer {
 
@@ -78,10 +81,25 @@ object computer {
     case _ => Left(s"${computer.value} is not a valid operation code")
   }
 
+
+
+  def parseNumber[F[_]: Applicative](in: String)(implicit F: Raise[F, String]): F[Int] = {
+    if (in.matches("-?[0-9]+")) in.toInt.pure[F]
+    else F.raise(show"'$in' could not be parsed as a number")
+  }
+
+  def instruction2[F[+_]: Applicative](computer:Computer)(implicit F: Raise[F, String]): F[Instruction] = computer.value match {
+    case Right(99) => Halt(computer).pure[F]
+    case Right(1) => Sum(computer).pure[F]
+    case Right(2) => Multiplication(computer).pure[F]
+    case _ => F.raise(s"${computer.value} is not a valid operation code")
+  }
+
+
   // A note:
   // Probably using State is an overkill, and using monad transformers even more, but, just for testing purposes
-  def go: ComputerState[Result[Int]] = {
-    (for {
+  def go: EitherT[ComputerState, String, Int] = {
+    for {
       inst <- EitherT(State.inspect(instruction))
       outInt <- EitherT[ComputerState, String, Int](
         inst match {
@@ -89,15 +107,22 @@ object computer {
           case op: Operation => op.result match {
             case Right(comp) => for {
               _ <- State.set[Computer](comp)
-              res <- go //
+              res <- go.value //
             } yield res
             case Left(l) => State.pure(Left(l))
           }
         }
       )
-    } yield outInt).value
+    } yield outInt
   }
 
-  def execute(computer: Computer): Result[Int] = go.runA(computer).value
+  def go2[F[+_]:Applicative, G[_]:Monad](implicit S: Stateful[G, Computer], E: Raise[F, String]): G[F[Instruction]] = {
+    (for {
+      inst <- S.inspect(instruction2[F])
+    } yield inst)
+  }
+
+  def execute(computer: Computer): Result[Int] = go.value.runA(computer).value
+
 
 }
